c32ecff39cdf3d1566202128e2be4b743b5115dc
lozoraitiskristupas <lozoraitiskristupas@outlook.com>
2024-04-12 21:08:11 +0300

    Added foreign keys

--- server-side/Basketball.Domain/Data/Entities/ExerciseProgress.cs
+++ server-side/Basketball.Domain/Data/Entities/ExerciseProgress.cs
@@ -10,8 +10,11 @@ namespace Basketball.Domain.Data.Entities
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
+        public User user { get; set; } = null!;
         public Guid TrainingPlanId { get; set; }
+        public TrainingPlan TrainingPlan { get; set; } = null!;
         public Guid ExerciseId { get; set; }
+        public Exercise Exercise { get; set; } = null!;
         public string ProgressVideoUrl { get; set; } = null!;
     }
 }
