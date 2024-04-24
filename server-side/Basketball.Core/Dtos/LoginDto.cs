426d9904a9b7bf68c99b7f659dc4057665779cc3
siciovas <rokas.siciovas@inbox.lt>
2024-04-24 15:53:07 +0300

    validations for login and registration

--- server-side/Basketball.Core/Dtos/LoginDto.cs
+++ server-side/Basketball.Core/Dtos/LoginDto.cs
@@ -1,8 +1,14 @@
-﻿namespace Basketball.Core.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Basketball.Core.Dtos
 {
     public class LoginDto
     {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Neteisingas el. paštas")]
         public required string Email { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "Slaptažodis turi būti bent iš 5 simbolių")]
         public required string Password { get; set; }
     }
 }
--- server-side/Basketball.Core/Dtos/RegisterDto.cs
+++ server-side/Basketball.Core/Dtos/RegisterDto.cs
@@ -1,23 +1,49 @@
 ﻿using Basketball.Domain.Data.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Basketball.Core.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [RegularExpression(@"^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ]*$", ErrorMessage = "Specialūs simboliai vardo laukelyje negalimi!")]
         public required string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ]*$", ErrorMessage = "Specialūs simboliai pavardės laukelyje negalimi!")]
         public required string Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+?(\d{11}|\d{9})$", ErrorMessage = "Telefono numeris gali prasdėti arba + arba 8")]
         public required string PhoneNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Neteisingas el. paštas")]
         public required string Email { get; set; }
+
         public DateOnly BirthDate { get; set; }
+
         public Role Role { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Slaptažodis turi būti bent iš 5 simbolių")]
         public required string Password { get; set; }
+
         public Gender? Gender { get; set; }
+
         public byte[]? Avatar { get; set; }
 
         // Fields for student
+        [RegularExpression(@"^-?\d*\.?\d+$", ErrorMessage = "Galimi tik skaičiai")]
         public int? Height { get; set; }
+
+        [RegularExpression(@"^-?\d*\.?\d+$", ErrorMessage = "Galimi tik skaičiai")]
         public double? Weight { get; set; }
+
+        [RegularExpression(@"^-?\d*\.?\d+$", ErrorMessage = "Galimi tik skaičiai")]
         public double? FootSize { get; set; }
+
+        [RegularExpression(@"^-?\d*\.?\d+$", ErrorMessage = "Galimi tik skaičiai")]
         public int? MetabolicAge { get; set; }
 
         // Fields for coach
