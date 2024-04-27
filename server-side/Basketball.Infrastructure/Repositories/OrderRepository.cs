5c6784294952a274460c76bd1b93299c0fb203dc
lozoraitiskristupas <lozoraitiskristupas@outlook.com>
2024-04-27 14:47:42 +0300

    Fix include bug

--- server-side/Basketball.Infrastructure/Repositories/OrderRepository.cs
+++ server-side/Basketball.Infrastructure/Repositories/OrderRepository.cs
@@ -40,9 +40,10 @@ namespace Basketball.Infrastructure.Repositories
         {
             return await _db.Orders
                             .Include(t => t.TrainingPlan)
+                            .ThenInclude(u => u.Coach)
+                            .Include(t => t.TrainingPlan)
                             .ThenInclude(x => x.Skills)
                             .ThenInclude(x => x.Exercises)
-                            .ThenInclude(u => u.Coach)
                             .Where(o => o.UserId == userId)
                             .ToListAsync();
         }
